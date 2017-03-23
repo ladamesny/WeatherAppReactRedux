c1d36d4b711c09365caf1eb1cca626c0f506c43d
ladamesny <adames.larry@gmail.com>
2017-03-22 21:55:55 -0400

    we wrote ajax call to weather api and wired up response to reducer

---
+++ src/actions/index.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const API_KEY = "cbd334be257e4506b1a03259172303";
+const rootUrl = `http://api.apixu.com/v1/forecast.json?key=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+  const url = `${rootUrl}&q=${city}`;
+  const request = axios.get(url);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  };
+}
